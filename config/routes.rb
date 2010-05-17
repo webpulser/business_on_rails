ed9e86eea9fb5a8a65b1875ea74a0138bc8cfec9
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-05-17 20:28:04 +0200

    config: add site routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :sites
+
+  map.namespace :admin do |admin|
+    admin.resources :sites, :member => { :activate => :post, :duplicate => :get}
+    admin.resources "site_categories", :controller => 'categories', :requirements => { :type => "site_category" }
+  end
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
