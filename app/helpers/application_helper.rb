32dbce0807282ee4e086f5f83587d21fba4c88ff
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-05-18 20:15:44 +0200

    site: display if a field is mandatory

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def label_with_required(form, label, method, errors, html_options={})
+    l = label
+    l += " #{t("required")}" unless errors.on(method).nil?
+    return form.label method, l, html_options
+  end
 end
