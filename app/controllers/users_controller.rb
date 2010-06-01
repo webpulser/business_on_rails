14513f59789298f0954e31cee6c29f05112635b6
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-06-01 20:13:31 +0200

    user: allow to modify their own informations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
+  before_filter :get_user, :only => [:edit, :update]
+
+  def show
+    redirect_to :action => 'edit'
+  end
+
+  def update
+  end
 
   def logins
     render :layout => false
+  end
+
+private
+  def get_user
+    if @user.nil?
+      redirect_to login_path
+    else
+      @user = current_user
+    end
+
   end
 
 end
