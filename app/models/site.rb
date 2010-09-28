7e3e5045f669069dee01a24bd2d1d83007bd1997
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-09-28 09:39:32 +0200

    sit: update models

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,15 +2,27 @@ class Site < ActiveRecord::Base
   belongs_to :user
   translates :name, :url, :description
   validates_presence_of :name, :url
+  validates_uniqueness_of :url
   validates_associated :user
 
   acts_as_taggable
 
   has_and_belongs_to_many :site_categories, :readonly => true, :join_table => 'categories_elements', :foreign_key => 'element_id', :association_foreign_key => 'category_id'
   has_and_belongs_to_many_attachments
+  has_many :screenshots, :through => :attachment_links, :class_name => 'Screenshot', :source => :attachment, :order => :position
 
-  def activate
-    update_attribute(:active, !self.active)
+  include AASM
+  aasm_column :status
+  aasm_initial_state :active
+  aasm_state :disabled
+  aasm_state :active
+
+  aasm_event :activate do
+    transitions :to => :active, :from => :disabled
+  end
+
+  aasm_event :disable do
+    transitions :to => :disabled, :from => :active
   end
 
 end
--- app/models/site_category.rb
+++ app/models/site_category.rb
@@ -1,3 +1,4 @@
 class SiteCategory < Category
   has_and_belongs_to_many :elements, :join_table => 'categories_elements', :foreign_key => 'category_id', :association_foreign_key => 'element_id', :class_name => 'Site'
+  has_and_belongs_to_many :sites, :join_table => 'categories_elements', :foreign_key => 'category_id', :association_foreign_key => 'element_id'
 end
