5daddc33477e3b62b425a1769b635d1241e85d2f
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-06-27 10:58:42 +0200

    add missing plugins and update gitignore

---
+++ vendor/plugins/localized_dates/lib/localized_dates.rb
@@ -0,0 +1,4 @@
+require 'core_ext/date'
+require 'core_ext/datetime'
+require 'core_ext/time'
+require 'core_ext/time_with_zone'
---
+++ vendor/plugins/localized_dates/spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'activesupport'
+
+gem 'rspec'
+
+require 'localized_dates'
